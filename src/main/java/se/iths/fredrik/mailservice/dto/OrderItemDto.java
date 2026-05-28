631b9edc592f6597ad48fbfda0f9f4cefc2e380f
FredrikAlmen93 <fredrik.x.almen@gmail.com>
2026-05-28 15:04:38 +0200

    Added validation to Dto-classes

--- src/main/java/se/iths/fredrik/mailservice/dto/OrderItemDto.java
+++ src/main/java/se/iths/fredrik/mailservice/dto/OrderItemDto.java
@@ -1,11 +1,22 @@
 package se.iths.fredrik.mailservice.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemDto {
 
+    @NotBlank(message = "Product name cannot be blank")
     private String productName;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
     private int quantity;
+
+    @Min(value = 0, message = "Price must be non-negative")
     private double price;
 }
